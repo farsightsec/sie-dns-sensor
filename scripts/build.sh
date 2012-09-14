748e6091befa8e2bc66bd06362af0760c3cd60cf
Robert Edmonds <edmonds@isc.org>
2012-09-14 17:46:35 -0400

    scripts/build.sh: add release component

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -e
 
 VERSION="$1"
+RELEASE="$2"
 
 if [ -z "$VERSION" ]; then
-    echo "Usage: $0 <VERSION>"
+    echo "Usage: $0 <VERSION> [<RELEASE>]"
     exit 1
 fi
 
-./scripts/nmsg_build.sh "$VERSION"
-./scripts/pkg_build.sh "$VERSION"
+if [ -z "$RELEASE" ]; then
+    RELEASE="1"
+fi
+
+./scripts/nmsg_build.sh "$VERSION" "$RELEASE"
+./scripts/pkg_build.sh "$VERSION" "$RELEASE"
